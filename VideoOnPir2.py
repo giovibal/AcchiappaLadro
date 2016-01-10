722bebbc2b83eafb31bd645af179f1f3b3f95ec7
Giovanni Baleani <giovibal@gmail.com>
2016-01-10 02:01:09 +0100

    Ok Prototype, ready for on field testing

--- VideoOnPir2.py
+++ VideoOnPir2.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
-import time
 import datetime
 import paho.mqtt.publish as publish
 import picamera
 import io
 import subprocess
+import sys
 
 sensor = 4
 
@@ -13,6 +13,9 @@ GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
 previous_state = False
 current_state = False
+
+previous_datetime = datetime.datetime.now()
+current_datetime = datetime.datetime.now()
 
 def write_photo(camera, timestamp):
     filename = 'photo_%s.jpg' % timestamp
@@ -46,26 +49,41 @@ def write_video(stream, timestamp):
     print('Uploading video %s started' % filename)
 
 with picamera.PiCamera() as camera:
-    stream = picamera.PiCameraCircularIO(camera, seconds=1)
-    # camera.resolution = (640, 480)
+    stream = picamera.PiCameraCircularIO(camera, seconds=2)
+
+    # Turn the camera's LED off
+    camera.led = False
+
+    camera.resolution = (1280, 768)
+    camera.iso = 800
     # camera.framerate = 10
-    camera.hflip = True
-    camera.vflip = True
+    # camera.hflip = True
+    # camera.vflip = True
     camera.start_recording(stream, format='h264', quality=20)
     try:
         while True:
             # time.sleep(0.1)
-            camera.wait_recording(1)
+            # camera.wait_recording(1)
+            camera.wait_recording(0.1)
             previous_state = current_state
             current_state = GPIO.input(sensor)
             if current_state != previous_state:
                 new_state = "HIGH" if current_state else "LOW"
-
                 ts_str = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
                 print("%s GPIO pin %s is %s" % (ts_str, sensor, new_state))
 
                 if new_state == "HIGH":
                     print("%s Presenza rilevata !" % ts_str)
+
+                    try:
+                        current_datetime = datetime.datetime.now()
+                        time_delta = current_datetime - previous_datetime
+                        previous_datetime = current_datetime
+                        msg = "Presenza rilevata - %s (%s tempo passato: %s)" % (ts_str, new_state, time_delta)
+                        publish.single("/baleani/laspio/pir1", msg, hostname="iot.eclipse.org", retain=True)
+                    except:
+                        print("Error with publish on mqtt: ", sys.exc_info()[0])
 
                     # infrared lamp on
                     # turn_light_on()
@@ -74,19 +92,26 @@ with picamera.PiCamera() as camera:
 
                     # Keep recording for 10 seconds and only then write the
                     # stream to disk
-                    camera.wait_recording(20)
+                    camera.wait_recording(10)
                     print("%s Registrazione completata per il video" % ts_str)
                     write_video(stream, ts_str)
 
                 # else:
                 #     if new_state == "LOW":
-                #         print("%s Registrazione completata per il video" % ts_str)
-                #         write_video(stream, ts_str)
+                #         try:
+                #             current_datetime = datetime.datetime.now()
+                #             time_delta = current_datetime - previous_datetime
+                #             previous_datetime = current_datetime
+                #             msg = "Nessuno - %s (%s tempo passato: %s)" % (ts_str, new_state, time_delta)
+                #             publish.single("/baleani/laspio/pir1", msg, hostname="iot.eclipse.org", retain=True)
+                #         except:
+                #             print "Error with publish on mqtt: ", sys.exc_info()[0]
+
+
 
 
     finally:
         camera.stop_recording()
         GPIO.cleanup()
 
-print("Closing pirtest2 session ...")
 
