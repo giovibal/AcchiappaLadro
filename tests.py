a152e35b9d1b94e59fc1b7620b586f9a1941c2f8
Giovanni Baleani <giovibal@gmail.com>
2016-11-05 23:47:37 +0100

    2 thread

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import pir
 import cam
 import configparser
+from multiprocessing import Pool
+
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read('config.ini')
@@ -11,14 +13,29 @@ mqtt_topic = config['mqtt']['mqtt_topic']
 local_tmp_dir = config['gdrive']['local_tmp_dir']
 gdriveCMD = config['gdrive']['cmd']
 
+print("initializing pir ... ")
 presence = pir.Pir(pir_id=pir_id
                    , mqtt_host=mqtt_host
                    , mqtt_topic=mqtt_topic)
+
+print("initializing cam ... ")
 camera = cam.Cam(pir_id=pir_id
                  , mqtt_host=mqtt_host
                  , mqtt_topic=mqtt_topic
                  , local_tmp_dir=local_tmp_dir
                  , gdrive_cmd=gdriveCMD)
 
-presence.start()
-camera.start()
+
+def start_pir():
+    print("initializing pir ... ")
+    presence.start()
+
+
+def start_cam():
+    print("initializing pir ... ")
+    camera.start()
+
+pool = Pool(processes=2)
+pool.apply_async(start_pir)
+pool.apply_async(start_cam)
+
