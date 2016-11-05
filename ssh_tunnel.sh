d6709dec50606f88a33da5a559d54662163bcd44
Giovanni Baleani <giovibal@gmail.com>
2016-11-05 11:03:48 +0100

    Execute permissions on init.d script, updated ssh_tunnel script, ignored movie and photo files

---
+++ ssh_tunnel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cloud_ssh_host = $1
+cloud_public_port = $2
+
+while true; do
+        ssh -o ConnectTimeout=30 \
+            -o BatchMode=yes \
+            -o TCPKeepAlive=yes \
+            -o ServerAliveInterval=240 \
+            -o ExitOnForwardFailure=yes \
+            -o UserKnownHostsFile=/dev/null \
+            -o StrictHostKeyChecking=no \
+            -i \
+            /home/pi/.ssh/id_rsa -N -R 0.0.0.0:${cloud_public_port}:localhost:22 ${cloud_ssh_host} &> /dev/null
+        sleep 15
+done;
